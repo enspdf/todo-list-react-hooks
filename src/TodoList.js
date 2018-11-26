c8d7eb6bdb7ee1a19d32ced068084dfe80278c3f
Sebastian Higuita <sebastianhc1995@hotmail.com>
2018-11-25 23:56:06 -0500

    Todo List With React Hooks

---
+++ src/TodoList.js
@@ -0,0 +1,33 @@
+import React from "react";
+import {
+  List,
+  ListItem,
+  ListItemSecondaryAction,
+  ListItemText,
+  Checkbox,
+  IconButton
+} from "@material-ui/core";
+import {Delete as DeleteIcon} from "@material-ui/icons";
+
+const TodoList = ({ todos, deleteTodo }) => (
+  <List>
+    {todos.map((todo, index) => (
+      <ListItem key={index.toString()} dense button>
+        <Checkbox tabIndex={-1} disableRipple />
+        <ListItemText primary={todo} />
+        <ListItemSecondaryAction>
+          <IconButton
+            aria-label="Delete"
+            onClick={() => {
+              deleteTodo(index);
+            }}
+          >
+            <DeleteIcon />
+          </IconButton>
+        </ListItemSecondaryAction>
+      </ListItem>
+    ))}
+  </List>
+);
+
+export default TodoList;
